5e9884c1a822dbe1693a5fff62e98cd25bb4574b
Joao Donaton <joao.vitor.donaton@gmail.com>
2025-10-26 19:48:56 -0400

    merge terminal commands and fix cryo texture

--- src/views/cryoRoom/CryoRoomView.js
+++ src/views/cryoRoom/CryoRoomView.js
@@ -75,7 +75,7 @@ class CryoView extends View{
             let index = i + (4*num);
             let newChamber;
             if(index <= GS.getDeaths()) {
-                newChamber = new CryoChamber(0.5+(4*(i-1)), 3.6, 1.8, `emptyCryo${i}`, (obj) => {
+                newChamber = new CryoChamber(0.5+(4*(i-1)), 3.6, 0.9, `emptyCryo${i}`, (obj) => {
                     this.textNotificationHandler.addText('This Cryo Chamber does not appear to have a person inside of it anymore....')
                 });
             }
