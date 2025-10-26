d7536395fbb3bc960f3f9a178b6628286df271d0
bsainiak <149976233+bsainiak@users.noreply.github.com>
2025-10-26 14:31:18 -0400

    Update ComputerView.js
    added audio

--- src/views/ComputerView.js
+++ src/views/ComputerView.js
@@ -395,6 +395,11 @@ class ComputerView extends View {
     }
 
     onEnter() {
+        
+        if (!AM.isLooping("startGame")) {
+            AM.stopAll()
+            AM.loop("startGame");      
+        }
         R.add(this.background);
         R.add(this.pinpad);
         R.add(this.terminalHighlight);
