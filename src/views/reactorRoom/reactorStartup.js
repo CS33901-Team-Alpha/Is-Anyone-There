0142c14bd1942aa31e6fb3e5b1fd6e543f5bbd9b
brandon-renner <brenner3@kent.edu>
2025-11-20 17:52:47 -0500

    tweaked asset for engine room door

--- src/views/reactorRoom/reactorStartup.js
+++ src/views/reactorRoom/reactorStartup.js
@@ -6,7 +6,7 @@ class ReactorStartupView extends View {
 
         this.textHandler = new TextNotificationHandler(0.5, 1); 
 
-        this.door = new StandaloneSlidingDoor(2, 2.7, 1, () => {}, true, 2, null, 7, 0, () => true);
+        this.door = new StandaloneSlidingDoor(4.75, 2.6, 1, () => {}, true, 2, null, 7, 0, () => true);
 
         // reactor simon-says color zones
         this.colors = [
